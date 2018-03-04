c4f159e7895a8e76831b86ed22db62dc65dba331
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-03-04 01:23:00 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,24 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+function addElementToBeginningOfArray(a, n){
+  return [n,...a]
+}
+function destructivelyAddElementToBeginningOfArray(a,n){
+  a.unshift(n)
+  return a
+}
+
+function addElementToEndOfArray(a,n){
+    return [...a,n]
+}
+function destructivelyAddElementToEndOfArray(a,n){
+  a = [...a,n]
+  return a
+}
+
+function accessElementInArray(a,n){
+  return a[n]
+}
+function destructivelyRemoveElementFromBeginningOfArray(a){
+  a = a.shift()
+  return a
+}
